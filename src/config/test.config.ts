c9a4394b0559c7f0b763700f195a70f0c5887e87
chathurperera <chathuraperera007@gmail.com>
2023-07-24 22:49:08 +0530

    chore: removed unnecessary code

--- src/config/test.config.ts
+++ src/config/test.config.ts
@@ -1,7 +1,5 @@
 import '@testing-library/jest-native/extend-expect';
 
-// jest.mock('@react-native-firebase/auth');
-
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
 
 jest.mock('react-native-gesture-handler');
